09932979dbf11035453f05facf36afc945f03713
Juuso Kantonen <juuso.kantonen@vaadin.com>
2023-05-02 13:24:42 +0300

    Improvements to FilterMasterDetailView responsiveness

--- src/main/java/com/example/application/views/filtermasterdetail/FilterMasterDetailView.java
+++ src/main/java/com/example/application/views/filtermasterdetail/FilterMasterDetailView.java
@@ -5,6 +5,8 @@ import com.example.application.views.itemview.ItemView;
 import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.checkbox.CheckboxGroup;
 import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
@@ -14,6 +16,8 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
@@ -21,6 +25,7 @@ import com.vaadin.flow.component.splitlayout.SplitLayoutVariant;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoIcon;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 
 import java.util.ArrayList;
@@ -31,6 +36,8 @@ public class FilterMasterDetailView extends SplitLayout {
 
     private int browserWidth;
     private Div filterColumn;
+    private Button toggleFiltersBtn;
+    private Button clearFiltersBtn;
 
     private Div masterColumn;
     private SplitLayout masterDetailLayout;
@@ -45,6 +52,7 @@ public class FilterMasterDetailView extends SplitLayout {
     public FilterMasterDetailView() {
         setSizeFull();
         addThemeVariants(SplitLayoutVariant.LUMO_SMALL);
+        addClassName("outer-split");
 
         filterColumn = new Div(createFilters());
         filterColumn.setMinWidth("4rem");
@@ -60,20 +68,13 @@ public class FilterMasterDetailView extends SplitLayout {
         itemView = new ItemView();
         itemView.closeBtn.addClickListener(e -> {
             if (browserWidth < 800) {
-                setSplitterPosition(100);
-            }
-        });
-        itemView.messagesBtn.addClickListener(e -> {
-            if (browserWidth < 800) {
-                masterDetailLayout.setSplitterPosition(0);
-            } else if (browserWidth < 1400 && getUI().get().hasClassName("detail-closed")) {
-                masterDetailLayout.setSplitterPosition(65);
-                getUI().get().removeClassName("detail-closed");
+                filterColumn.setVisible(true);
+                masterDetailLayout.setSplitterPosition(100);
             } else if (browserWidth < 1400) {
                 masterDetailLayout.setSplitterPosition(100);
-                getUI().get().addClassName("detail-closed");
             }
         });
+        itemView.messagesBtn.setVisible(false);
         masterDetailLayout.addToSecondary(itemView);
 
         addToSecondary(masterDetailLayout);
@@ -100,32 +101,39 @@ public class FilterMasterDetailView extends SplitLayout {
 
     private void initResponsiveRules() {
         if (browserWidth < 800) {
-            setSplitterPosition(100);
+            setOrientation(Orientation.VERTICAL);
+            setSplitterPosition(0);
+            getUI().get().addClassName("filters-collapsed");
+            toggleFiltersBtn.setIcon(LumoIcon.ANGLE_DOWN.create());
             masterDetailLayout.setSplitterPosition(100);
             getUI().get().addClassName("detail-closed");
             itemView.setMinWidth("");
             itemView.setMaxWidth("");
+            filterColumn.setMinHeight("3rem");
             filterColumn.setMaxWidth("");
             itemView.closeBtn.setVisible(true);
-            itemView.messagesBtn.setVisible(true);
         } else if (browserWidth < 1400) {
+            setOrientation(Orientation.HORIZONTAL);
             setSplitterPosition(25);
+            getUI().get().removeClassName("filters-collapsed");
+            toggleFiltersBtn.setIcon(LumoIcon.ANGLE_LEFT.create());
             masterDetailLayout.setSplitterPosition(100);
             getUI().get().addClassName("detail-closed");
             itemView.setMinWidth("");
             itemView.setMaxWidth("");
             filterColumn.setMaxWidth("24rem");
-            itemView.closeBtn.setVisible(false);
-            itemView.messagesBtn.setVisible(true);
+            itemView.closeBtn.setVisible(true);
         } else {
+            setOrientation(Orientation.HORIZONTAL);
             setSplitterPosition(25);
+            getUI().get().removeClassName("filters-collapsed");
+            toggleFiltersBtn.setIcon(LumoIcon.ANGLE_LEFT.create());
             masterDetailLayout.setSplitterPosition(50);
             getUI().get().removeClassName("detail-closed");
             itemView.setMinWidth("4rem");
-            itemView.setMaxWidth("30rem");
+            itemView.setMaxWidth("50vw");
             filterColumn.setMaxWidth("24rem");
             itemView.closeBtn.setVisible(false);
-            itemView.messagesBtn.setVisible(false);
         }
 
 
@@ -133,7 +141,41 @@ public class FilterMasterDetailView extends SplitLayout {
 
     private Component createFilters() {
         H3 filtersHeading = new H3("Filters");
-        filtersHeading.addClassName(LumoUtility.Padding.Bottom.LARGE);
+        filtersHeading.setWidthFull();
+        clearFiltersBtn = new Button("Clear");
+        clearFiltersBtn.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+        clearFiltersBtn.addClassName("clear-button");
+        clearFiltersBtn.setEnabled(false);
+
+        toggleFiltersBtn = new Button(LumoIcon.ANGLE_LEFT.create());
+        toggleFiltersBtn.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        toggleFiltersBtn.addClickListener(e -> {
+            if (getUI().get().hasClassName("filters-collapsed")) {
+                getUI().get().removeClassName("filters-collapsed");
+                if (browserWidth < 800) {
+                    setSplitterPosition(80);
+                    toggleFiltersBtn.setIcon(LumoIcon.ANGLE_UP.create());
+                } else {
+                    setSplitterPosition(25);
+                    toggleFiltersBtn.setIcon(LumoIcon.ANGLE_LEFT.create());
+                }
+            } else {
+                getUI().get().addClassName("filters-collapsed");
+                setSplitterPosition(0);
+                if (browserWidth < 800) {
+                    toggleFiltersBtn.setIcon(LumoIcon.ANGLE_DOWN.create());
+                } else {
+                    toggleFiltersBtn.setIcon(LumoIcon.ANGLE_RIGHT.create());
+                }
+            }
+        });
+        FlexLayout filtersHeader = new FlexLayout(filtersHeading, clearFiltersBtn, toggleFiltersBtn);
+        filtersHeader.setAlignItems(FlexComponent.Alignment.CENTER);
+        filtersHeader.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        filtersHeader.addClassNames(LumoUtility.Padding.Bottom.LARGE, LumoUtility.Gap.SMALL);
+        filtersHeader.setWidthFull();
+        filtersHeader.addClassName("filters-header");
+
         H4 productHeading = new H4("Product");
         MultiSelectComboBox<String> categoryFilter = new MultiSelectComboBox<>("Category");
         categoryFilter.setItems("Keyboards", "Mice", "Monitors");
@@ -155,10 +197,11 @@ public class FilterMasterDetailView extends SplitLayout {
         featuresFilter.setWidthFull();
 
         VerticalLayout filtersContent = new VerticalLayout();
+        filtersContent.addClassName("filters-content");
         filtersContent.setSizeFull();
         filtersContent.setPadding(true);
         filtersContent.setSpacing(false);
-        filtersContent.add(filtersHeading, productHeading, categoryFilter, brandFilter, modelFilter, stockFilter, specificationsHeading, featuresFilter);
+        filtersContent.add(filtersHeader, productHeading, categoryFilter, brandFilter, modelFilter, stockFilter, specificationsHeading, featuresFilter);
 
         return filtersContent;
     }
@@ -173,6 +216,14 @@ public class FilterMasterDetailView extends SplitLayout {
         Grid<Item> grid = new Grid();
         grid.addColumn(Item::getItem).setHeader("Items");
         grid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
+        grid.addItemClickListener(e -> {
+            if (browserWidth < 800) {
+                masterDetailLayout.setSplitterPosition(0);
+                filterColumn.setVisible(false);
+            } else if (browserWidth < 1400) {
+                masterDetailLayout.setSplitterPosition(0);
+            }
+        });
 
         ArrayList<Item> items = new ArrayList();
         for (int i = 0; i < 6; i++) {
